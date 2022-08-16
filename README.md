c7944c88e04431eae10a8c838d064f25855e28f3
Schaffer <mr.giancarloschaffer@gmail.com>
2022-08-16 18:00:13 -0300

    Tio Lu bot

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Bot do tio Lú, ele responde por você
+
+## Comandos
+
+no - No. Nope. Não. Nie. Não mesmo. Sério, não.
+
+sentiu - Diga lá, Lú
+
+saoko - Chica, ¿qué dices? Olha que eu hablo
+
+bait - Wow, nice bait
+
+hahaha - HAHAHAHA KKKKK ASUHASHUHUAS
+
+quit - Até logo, até mais ver, bon voyage
--- main.py
+++ main.py
@@ -1,91 +1,109 @@
 import telebot
 from decouple import config
-from datetime import datetime
-from pydub import AudioSegment
-from io import BytesIO
-from pydub.utils import which
-import pytz
-from gtts import gTTS
-
-
-AudioSegment.converter = which("ffmpeg")
+from random import randrange
 
 API_KEY = config("TOKEN_TELEGRAM")
 
 bot = telebot.TeleBot(API_KEY)
 
 
-def semana_encerrada(date):
-    msg_date = datetime.fromtimestamp(date, pytz.timezone('America/Sao_Paulo'))
-
-    num = msg_date.weekday()
-
-    sem = ("uma Segunda-feira", "uma Terça-feira", "uma Quarta-feira",
-           "uma Quinta-feira", "uma Sexta-feira", "um Sábado", "um Domingo")
-
-    def getPeriod(h):
-        if h == 0:
-            return ''
-        if h <= 3:
-            return ' da madrugada'
-        if h < 12:
-            return ' da manhã'
-        if h < 19:
-            return ' da tarde'
-        return ' da noite'
-
-    def getHour(h):
-        if h == '00':
-            return 'meia-noite'
-        if h == '01' or h == '13':
-            return 'Uma hora'
-        return f"{msg_date.strftime('%I')} horas".lstrip("0")
-
-    current_time = msg_date.strftime(
-        f"{getHour(msg_date.strftime('%H'))}{getPeriod(int(msg_date.strftime('%H')))} de {sem[num]}")
-
-    return current_time
+def get_message_id(msg):
+    print('has reply', msg.reply_to_message)
+    try:
+        return msg.reply_to_message.message_id
+    except (NameError, AttributeError) as e:
+        return msg.message_id
 
 
-def get_audio(date):
-    mp3_fp = BytesIO()
-    tts = gTTS(semana_encerrada(date), lang='pt')
-    tts.write_to_fp(mp3_fp)
-    mp3_fp.seek(0)
+@bot.message_handler(commands=["saoko"])
+def saoko(msg):
+    number = randrange(1, 5)
+    saoko = open(f'./assets/saoko/{number}.mp4', 'rb')
+    que_dices = open('./assets/que_dices.mp3', 'rb')
 
-    sao = AudioSegment.from_file("./assets/sao.mp3")
-    horas = AudioSegment.from_mp3(mp3_fp)
-    encerrada = AudioSegment.from_file("./assets/encerrada.mp3")
-
-    semana = sao + horas + encerrada
-    mp3IO = BytesIO()
-    semana.export(mp3IO, format="mp3")
-    return mp3IO.getvalue()
-
-
-def que_horas_sao(date):
-    msg_date = datetime.fromtimestamp(date, pytz.timezone('America/Sao_Paulo'))
-    num = msg_date.weekday()
-
-    if num == 2 and msg_date.strftime('%H') == '16':
-        semana = AudioSegment.from_file("./assets/semana.mp3")
-        mp3IO = BytesIO()
-        semana.export(mp3IO, format="mp3")
-        return mp3IO.getvalue()
+    if msg.text.find('hablo') != -1:
+        bot.send_audio(msg.chat.id, que_dices,
+                       performer='Rosalía', title='Chica, ¿qué dices?', reply_to_message_id=get_message_id(msg))
     else:
-        return get_audio(date)
+        bot.send_animation(
+            msg.chat.id, animation=saoko, reply_to_message_id=get_message_id(msg))
 
 
-@bot.message_handler(commands=["encerrada"])
-def responder(msg):
-    print('Quem perguntou?', msg.from_user.first_name)
-    # To send an Audio File
-    bot.send_audio(msg.chat.id, que_horas_sao(msg.date),
-                   performer='@a_semana_bot', title='São que horas?')
+@bot.message_handler(commands=["hahaha"])
+def hahaha(msg):
+    hahaha = open('./assets/hahaha.mp3', 'rb')
+    risadas = ['hahaha', 'kkkkk', 'rsrsrs', 'ashuahus']
+    number = randrange(len(risadas))
+    bot.send_audio(msg.chat.id, hahaha,
+                   title=risadas[number], reply_to_message_id=get_message_id(msg))
 
-    # To send a Message File
-    # bot.reply_to(
-    #     msg, f"São {semana_encerrada(msg.date)}, Ahh... Semana praticamente encerrada!")
 
+@bot.message_handler(commands=["quit"])
+def hahaha(msg):
+    quit = open('./assets/quit.png', 'rb')
+    byes = ["Até logo", "Até mais ver", "Bon voyage", "Arrivederci", "Até mais",
+            "Adeus", "Boa viagem", "Vá em paz", "Que a porta bata onde o sol não bate"]
+    number = randrange(len(byes))
+    bot.send_photo(
+        msg.chat.id, photo=quit, reply_to_message_id=msg.message_id, caption=byes[number])
+
+
+@bot.message_handler(commands=["no"])
+def no(msg):
+    no = open('./assets/no.mp4', 'rb')
+    bot.send_animation(
+        msg.chat.id, animation=no, reply_to_message_id=get_message_id(msg))
+
+
+@bot.message_handler(commands=["bait"])
+def bait(msg):
+    bait = open('./assets/bait.jpg', 'rb')
+    bot.send_photo(
+        msg.chat.id, photo=bait, reply_to_message_id=get_message_id(msg))
+
+
+@bot.message_handler(commands=["sentiu"])
+def bait(msg):
+    sentiu = open('./assets/sentiu.png', 'rb')
+
+    bot.send_message(
+        msg.chat.id, "Galvão...")
+    bot.send_message(
+        msg.chat.id, "Diga lá, Tino.")
+    bot.send_photo(
+        msg.chat.id, photo=sentiu, reply_to_message_id=get_message_id(msg))
+
+# @bot.message_handler(commands=["google"])
+# def google(msg):
+#     print(msg)
+#     print('=========================================')
+#     try:
+#         googled = msg.reply_to_message.text.replace(
+#             '\n', ' ').replace(' ', '+')
+#         url = f"vamos lá, nem é tão complicado assim...\nhttps://letmegooglethat.com/?q={googled}"
+#         bot.send_message(
+#             msg.chat.id, text=url, reply_to_message_id=msg.reply_to_message.message_id)
+#     except:
+#         bot.reply_to(
+#             msg, "???")
+
+# To send an Audio File
+# bot.send_audio(msg.chat.id, que_horas_sao(msg.date),
+#                performer='@tio_lu', title='Diga lá, Lu')
+
+# To send a Message File
+# bot.reply_to(
+#     msg, f"São {semana_encerrada(msg.date)}, Ahh... Semana praticamente encerrada!")
+
+
+# def alwaysTrue(msg):
+#     return True
+
+
+# @bot.message_handler(func=alwaysTrue)
+# def general(msg):
+#     print(msg)
+#     bot.reply_to(
+#         msg, f"{msg.text}?")
 
 bot.polling()
